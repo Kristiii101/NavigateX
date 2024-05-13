73aab9a65e427afd36e800df0efb78914ab4de8a
Karasin Kristian <karasinkristian@gmail.com>
2024-05-13 15:14:16 +0300

    Updated SearchAutoComplete.. with delete text button

--- Components/SearchAutoCompleteComponent.jsx
+++ Components/SearchAutoCompleteComponent.jsx
@@ -1,21 +1,26 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {StyleSheet ,View, TouchableOpacity, Text } from 'react-native';
 import googleapikey from '../utils/google_api_key';
-import {StyleSheet ,View } from 'react-native';
 
 export const SearchAutoCompleteComponent = ({handleLocationSearch}) => {
-  
-  const [searchQuery, setSearchQuery] = useState('');
+
+  const autocompleteRef = useRef(null);
 
   const handleSearch = (data) => {
     if (handleLocationSearch) {
       handleLocationSearch(data);
     }
   };
+
+  const clearInput = () => {
+    autocompleteRef.current.setAddressText('');
+  };
   
   return (
     <View style={styles.container}>
         <GooglePlacesAutocomplete
+        ref={autocompleteRef}
         placeholder='Search for a location'
         fetchDetails={true}
         GooglePlacesSearchQuery={{
@@ -27,23 +32,30 @@ export const SearchAutoCompleteComponent = ({handleLocationSearch}) => {
         }}
         query={{
             key: googleapikey,
-            language: 'en', 
+            language: 'ro', 
             types: 'geocode',
+            types: 'establishment',
+            location: '45.7494,21.2272',    // Coordinates for Timisoara
+            radius: 50000,                  // 50 km radius
+            rankby: 'distance',
         }}
         styles={{
             textInputContainer: {
-            backgroundColor: 'grey',
+            backgroundColor: 'white',
             },
             textInput: {
-            height: 38,
+            height: 35,
             color: '#5d5d5d',
-            fontSize: 16,
+            fontSize: 17,
             },
             predefinedPlacesDescription: {
             color: '#1faadb',
             },
         }}
         /> 
+        <TouchableOpacity style={styles.clearButton} onPress={clearInput}>
+          <Text style={styles.clearButtonText}>✕</Text>
+        </TouchableOpacity>
     </View>
   );
 };
@@ -51,14 +63,14 @@ export const SearchAutoCompleteComponent = ({handleLocationSearch}) => {
 const styles = StyleSheet.create({
     container: {
       position: 'absolute',
-      top: 40, // Adjust based on your header's height
-      left: 20,
-      right: 20,
+      top: 25, // Adjust based on your header's height
+      left: 15,
+      right: 15,
       zIndex: 1,
     },
     input: {
       height: 40,
-      backgroundColor: '#fff',
+      backgroundColor: '#white',
       borderRadius: 5,
       padding: 10,
       fontSize: 18,
@@ -67,6 +79,17 @@ const styles = StyleSheet.create({
       shadowColor: '#000',
       shadowOffset: { height: 1, width: 1 },
       elevation: 3, // for Android shadow
+    },
+    clearButton: {
+      position: 'absolute',
+      right: 10,
+      top: 10,
+      color: 'black',
+      backgroundColor: 'white',
+      zIndex: 2,
+    },
+    listView: {
+      backgroundColor: 'white',
     },
   });
 
